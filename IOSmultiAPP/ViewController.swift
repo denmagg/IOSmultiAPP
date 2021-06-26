a99e5456e969a5184085972aef5af31a9e12c64c
Denis <denmagg.work@gmail.com>
2021-06-27 02:08:22 +0300

    Initial Commit

---
+++ IOSmultiAPP/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  IOSmultiAPP
+//
+//  Created by Denis Medvedev on 27/06/2021.
+//  Copyright © 2021 Denis Medvedev. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
